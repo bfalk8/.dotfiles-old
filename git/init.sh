d67faeacf61dcc30ce9e230895051fe317174bbd
bfalk8 <bfalk8@gmail.com>
2015-12-15 17:59:26 -0800

    added git config

--- git/init.sh
+++ git/init.sh
@@ -9,9 +9,14 @@ die() {
     exit 1
 }
 
+# Link .gitconfig
+echo "Linking .gitconfig"
+for i in ~/.gitconfig; do [ -e $i ] && mv $i $i.old; done
+
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 
 # Run git init script
-echo "Running git init"t config --global user.email "you@example.com"
+echo "Running git init"
 git config --global user.email "bfalk@ucsd.edu"
 git config --global user.name "bfalk8"
 git config --global push.default simple
